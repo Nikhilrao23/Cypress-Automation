bce23700a2420f29437ded4e75a3411845b93fd1
Nikhilrao23 <raonikhil23@gmail.com>
2019-10-17 00:48:30 -0400

    Adding New Changes to the Framework

--- cypress/integration/summa/Gitkaga.js
+++ cypress/integration/summa/Gitkaga.js
@@ -1,14 +1,10 @@
-
 /// <reference types = "Cypress"/>
 
 describe("Test Framework", function(){
 
     before(function(){
 
-        cy.fixture('ghum').then(function(holi){
-
-            this.noli = holi
-        })
+        //Will run once before Test
 
     })
 
@@ -16,36 +12,11 @@ describe("Test Framework", function(){
 
         cy.visit("https://rahulshettyacademy.com/angularpractice/")
 
-        cy.get("input[name='name']:nth-child(2)").type(this.noli.name)
+        cy.get("input[name = 'name']:nth-child(2)").type("Nikhil")
 
-        cy.get("#exampleFormControlSelect1").select(this.noli.gender)
+        cy.get("#exampleFormControlSelect1").select("Female")
 
-        cy.get("input[name='name']:nth-child(1)").should("have.value", this.noli.name)
-
-        cy.get("#exampleFormControlSelect1").should("have.value", this.noli.gender)
 
         
-        cy.get("input[name='name']:nth-child(2)").should("have.attr", "minlength", "2")
-
-        cy.get("#inlineRadio3").should("be.disabled")
-
-        cy.get(':nth-child(2) > .nav-link').click()
-
-        // this.noli.productName.forEach(function(element) {
-
-        //     cy.addProduct(element)
-
-        //   });
-
-
     })
-
 })
-
-
-
-/*What we are planning to do
-Framework Building
-1. We will use Hooks/ Annotations
-
-*/
